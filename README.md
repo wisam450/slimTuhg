3e1462fb852fa287e5d521f6b7a01a674b348993
wisam <wisam.basheir@gmail.com>
2019-11-20 14:30:22 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+#  What is this?
+Get perfect shadows every time for the non-designer.
+# installation
+```javascript
+npm i softshadow
+```
+then ...
+```javascript
+import {  softshadow } from 'softshadow';
+softshadow({
+    shadow_type : 'soft',
+    padding: false
+})
+```
+# options
+Softshadow support 2 options, both of which are optional:
+* *shadow_type* - _hard | soft_ (Default to soft)
+* *padding* - _boolean_ (Default to false)
---
+++ index.js
@@ -0,0 +1,17 @@
+function softshadow(option){
+    let images = document.querySelectorAll('.softshadow');
+    if(option.shadow_type === 'hard'){
+        option.shadow_type = '15px'
+    }else{
+        option.shadow_type = '0px';
+    }
+    images.forEach((img)=> {
+        img.style.boxShadow = `5px 10px 10px ${option.shadow_type} rgba(0,0,0,0.22)`;
+        if(option.padding){
+            img.style.padding = '1em'
+        }
+        img.style.margin = '1em'
+    })
+}
+
+module.exports.softshadow = softshadow;
